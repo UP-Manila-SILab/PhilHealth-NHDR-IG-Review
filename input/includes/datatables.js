b061ed960faff2b78205036c21e381629f3c723d
Gian Balais <gctbalais@nhdr.gov.ph>
2025-09-12 14:28:44 +0800

    v1.0.54f
    - quick patch

--- input/includes/datatables.js
+++ input/includes/datatables.js
@@ -1,23 +1,21 @@
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const table = document.querySelectorAll("table");
-        if (table) {
-            table.forEach(function(element, index){
-                element.classList.add("display");
-                new DataTable(element, {
-                    paging: true,
-                    pageLength: 25,
-                    language: {
-                        lengthLabels: {
-                            '-1': 'Display All'
-                        }
-                    },
-                    lengthMenu: [25, 50, -1],
-                    searching: true,
-                    info: true,
-                    order: []
-                });
+document.addEventListener("DOMContentLoaded", function () {
+    const table = document.querySelectorAll("table");
+    if (table) {
+        table.forEach(function(element, index){
+            element.classList.add("display");
+            new DataTable(element, {
+                paging: true,
+                pageLength: 25,
+                language: {
+                    lengthLabels: {
+                        '-1': 'Display All'
+                    }
+                },
+                lengthMenu: [25, 50, -1],
+                searching: true,
+                info: true,
+                order: []
             });
-        }
-    });
-</script>
+        });
+    }
+});
